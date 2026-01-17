20140e8d85a2497d2a341e9c46238eb20356efb9
IceHermit <116965845+IceHermit@users.noreply.github.com>
2026-01-17 13:59:21 +0530

    Add files via upload

---
+++ preprocessor.py
@@ -0,0 +1,68 @@
+import pandas as pd
+from nltk import word_tokenize, sent_tokenize, pos_tag
+from nltk.stem import WordNetLemmatizer
+
+
+lemmatizer = WordNetLemmatizer()
+messages = {}
+
+def read_raw():
+    lines = []
+    with open("input.txt", 'r',  encoding='utf-8') as fin:
+        lines = fin.readlines()
+    return lines
+
+
+def remove_timestamp(lines):
+    modified = []
+    for line in lines:
+        modified.append(line.partition('-')[2])
+    return modified
+
+
+def categorize(lines):
+    for line in lines:
+        name, _, message = line.partition(':')
+        message = message.strip()
+        if name in messages:
+            messages[name].append(message)
+        else:
+            messages[name] = [message]
+
+
+def get_wordnet_pos(tag):
+    if tag.startswith('J'):  
+        return 'a'
+    elif tag.startswith('V'):  
+        return 'v'
+    elif tag.startswith('N'):  
+        return 'n'
+    elif tag.startswith('R'):  
+        return 'r'
+    else:
+        return 'n'  
+
+
+processed_data = {}
+
+def tokenize_and_shi(messages, name):
+    global processed_data
+    tokens = []
+    for message in messages:
+        sentences = sent_tokenize(message)
+        for sentence in sentences:
+            word_tokens = word_tokenize(sentence)
+            word_tokens = pos_tag(word_tokens)
+            for token in word_tokens:
+                tokens.append(lemmatizer.lemmatize(token[0], get_wordnet_pos(token[1])))
+                
+    processed_data[name] = tokens 
+
+if __name__ == "__main__":
+    categorize(remove_timestamp(read_raw()))
+    print("---")
+    for name in messages:
+        tokenize_and_shi(messages[name], name)
+    for name in processed_data:
+        print(f"Tokens from {name}")
+        print(processed_data[name])
