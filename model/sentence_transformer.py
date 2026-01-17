f3cecba29fc7a8270121bee75f02d9662407ccbb
Nabaneet-P <nabaneetpriyadarshi2202@gmail.com>
2026-01-18 00:44:12 +0530

    Update sentence_transformer.py

--- model/sentence_transformer.py
+++ model/sentence_transformer.py
@@ -19,6 +19,46 @@ def classify_embedding(text):
         cat: util.cos_sim(emb, cat_emb).item()
         for cat, cat_emb in cat_embeddings.items()
     }
-    return max(scores, key=scores.get),scores
+    return max(scores, key=scores.get)
+
+#Preprocessing 
+messages = {}
+
+def read_raw():
+    lines = []
+    with open("input.txt", 'r',  encoding='utf-8') as fin:
+        lines = fin.readlines()
+    return lines
+
+def remove_timestamp(lines):
+    modified = []
+    for line in lines:
+        modified.append(line.partition('-')[2])
+    return modified
+
+def categorize(lines):
+    for line in lines:
+        name, _, message = line.partition(':')
+        message = message.strip()
+        if name in messages:
+            messages[name].append(message)
+        else:
+            messages[name] = [message]
+
+categorize(remove_timestamp(read_raw()))
+
+for key,val in messages.items():
+    list_of_sentences=val
+    sentence=" ".join(list_of_sentences)
+
+    # Word Splitting
+    tokens = word_tokenize(sentence.lower())
+
+    # Stop Word Removal
+    stop_words = set(stopwords.words('english'))
+    filtered_tokens = [w for w in tokens if w not in stop_words and w.isalnum()]
+    characteristic=" ".join(filtered_tokens)
+
+    print(key, classify_embedding(characteristic))
 
 # classify_embedding("fear exactly figure")
