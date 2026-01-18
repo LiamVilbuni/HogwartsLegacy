d729e23cf10311017a8d3052451bf18bf910bcfe
Bhavi-pm <motwanibhavishya07@gmail.com>
2026-01-18 09:25:00 +0530

    Create integrated.py

---
+++ model/integrated.py
@@ -0,0 +1,66 @@
+import pandas as pd
+messages = {}
+
+
+def read_raw():
+    lines = []
+    with open("input.txt", 'r',  encoding='utf-8') as fin:
+        lines = fin.readlines()
+    return lines
+
+
+def remove_timestamp(lines):
+    modified = []
+    for line in lines:
+        modified.append(line.partition('-')[2])
+    return modified
+
+
+def categorize(lines):
+    for line in lines:
+        name, _, message = line.partition(':')
+
+        if (not len(_)):
+            continue
+        
+        message = message.strip()
+        if name in messages:
+            messages[name].append(message)
+        else:
+            messages[name] = [message]
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sentence_transformers import SentenceTransformer, util
+
+model = SentenceTransformer("all-MiniLM-L6-v2")
+
+category={"Gryffindor": "bold confident outspoken bravery nerve chivalry determination positive courage resilience moral outgoing good dependable",
+          "Ravenclaw": "analytical logical knowledge witty creative original wise eccentric intuitive aloof pretentious judgemental",
+          "Hufflepuff": "supportive loyal friendly fairness patience humility modesty tenacity just dedicated humble caring kind love amazing",
+          "Slytherin": "persuasive sharp cunning leadership ambition prejudiced arrogant selfish sneaky pragmatism negative manipulative callousness ruthlessness cruel bad vindictive"}
+
+cat_embeddings = {
+    cat: model.encode(text)
+    for cat, text in category.items()
+}
+
+def classify_embedding(text):
+    emb = model.encode(text)
+    scores = {
+        cat: util.cos_sim(emb, cat_emb).item()
+        for cat, cat_emb in cat_embeddings.items()
+    }
+    return max(scores, key=scores.get)
+
+
+if __name__ == "__main__":
+    categorize(remove_timestamp(read_raw()))
+    for person in messages:
+        freq={"Gryffindor":0, "Ravenclaw":0, "Hufflepuff":0, "Slytherin":0}
+        for message in messages[person]:
+            freq[classify_embedding(message)]+=1
+        max_freq=max(freq.values())
+        for h in freq:
+            if freq[h]==max_freq:
+                house=h
+                break
+        print(f"{person} : {house}")
