e5fa040fe7229d0602b859605c6ac461d167ae2b
LiamVilbuni <liamvilbuni@gmail.com>
2026-01-18 16:29:07 +0530

    Changed debug status

--- main.py
+++ main.py
@@ -55,4 +55,4 @@ def dated_url_for(endpoint, **values):
     return url_for(endpoint, **values)
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000, host='localhost')
+    app.run(debug=False, port=5000, host='localhost')
